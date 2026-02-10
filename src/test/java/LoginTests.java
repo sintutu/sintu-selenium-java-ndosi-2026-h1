b604b13e24a6c1f9eff4684e06a408244abf46c1
sintutu <160270685+sintutu@users.noreply.github.com>
2026-02-10 22:56:39 +0200

    Update comments

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,5 +1,5 @@
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import pages.*;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -8,19 +8,12 @@ public class LoginTests extends TestBase {
     @Test
     public void logIntoNdosiDevSite() throws InterruptedException {
 
-        // Making page objects inside my tests is gross.
+        // Why must my test know how to make page objects?
         HomePage homePage = new HomePage();
         PracticePage practicePage = new PracticePage();
         DashboardPage dashboardPage = new DashboardPage();
 
-        // Instantiate driver. But why in the test?
-        driver = new ChromeDriver();
-        // I get why the manage interface exists. It manages the driver environment.
-        driver.manage().window().maximize();
-
-        // I prefer navigate().to(baseUri) over driver.get(baseUri) because users don't think in terms of get requests
-        // They don't think in terms of navigation either. But this at least establishes they can hit the back button.
-        driver.navigate().to(baseUri);
+        driver.get(baseUri);
         assertThat(driver.findElement((homePage.heading)).getText())
                 .isEqualTo("Master Test Automation");
         driver.findElement(homePage.loginButton).click();
@@ -29,11 +22,9 @@ public class LoginTests extends TestBase {
         driver.findElement(practicePage.loginEmail).sendKeys(practicePage.username);
         driver.findElement(practicePage.loginPassword).sendKeys(practicePage.password);
         driver.findElement(practicePage.loginButton).click();
-        Thread.sleep(1000); // Added a wait. Didn't feel like adding a Wait.
-        assertThat(driver.findElement(dashboardPage.welcomeMessage).getText())
+        assertThat(webDriverWait
+                .until(ExpectedConditions.visibilityOfElementLocated(dashboardPage.welcomeMessage)).getText())
                 .isEqualTo("Welcome back, sintutu! \uD83D\uDC4B");
-
-        // I still need to quit that driver at the end of the test. I don't want that Chrome window hanging around.
     }
 }
 
@@ -43,10 +34,14 @@ public class LoginTests extends TestBase {
 * I'm organising my locators by pages
 * Yet things look like /#overview, /#practice, /#dashboard
 * Are these pages? Does the page object model apply?
+* After research, I see this is a Single Page App that uses hash routing
+* Page objects may not be the best way to model the UI.
 *
 * Asserts are hard to understand.
 * I was typing in assert and I didn't see a problem in the IDE.
 * I had to google to find testNG asserts until I found I should have used Assert.
+* Preferred fluent assertions from AssertJ. Worried about test runtime.
 *
 * Is the driver environment a whole browser window? Am I spinning up a whole new, RAM-hungry, Chrome window?
+* Yes.
 * */
--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -1,8 +1,31 @@
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+import java.time.Duration;
 
 public class TestBase {
-    // Declare private driver instance
+    // Declare WebDriver instance
     protected WebDriver driver;
 
+    // Declare WebDriverWait instance
+    protected WebDriverWait webDriverWait;
+
     protected final String baseUri = "https://ndosisimplifiedautomation.vercel.app/";
+
+    @BeforeTest
+    public void Setup(){
+        // Instantiate driver
+        driver = new ChromeDriver();
+        webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(1000));
+        // I get why the manage interface exists. It manages the driver environment.
+        driver.manage().window().maximize();
+    }
+
+    @AfterTest
+    public void Teardown(){
+        driver.quit();
+    }
 }
